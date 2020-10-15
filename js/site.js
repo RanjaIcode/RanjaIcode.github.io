d0a3ff16c843b3300fb23f521b46b0706c5c7502
RanjaIcode <ranjabosek@hotmail.com>
2020-10-15 15:13:09 +0200

    next

--- js/site.js
+++ js/site.js
@@ -77,11 +77,14 @@ function getLocation() {
   
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition, showError);
-    } else { 
+    } 
+    else { 
       x.innerHTML = "Geolocation is not supported by this browser.";
     }
   }
   
+ 
+
   function showPosition(position) {
     var x = document.getElementById("demo");
   
