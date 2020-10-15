9da2783840d51f1e160531bf103137ef2b279299
RanjaIcode <ranjabosek@hotmail.com>
2020-10-15 13:08:05 +0200

    next

--- pwa_sw.js
+++ pwa_sw.js
@@ -22,7 +22,8 @@ if ("serviceWorker" in navigator) {
                             "lib/jquery/dist/jquery.js",
                             "lib/bootstrap/dist/js/bootstrap.bundle.js",
                             "js/site.js",
-                            "index.html"
+                            "index.html",
+                            "/"
                           ];
 
   self.addEventListener('install', function(event){
@@ -40,27 +41,27 @@ if ("serviceWorker" in navigator) {
   });
 
 
-// self.addEventListener('activate', function (event) {
-//     // `claim()` sets this worker as the active worker for all clients that
-//     // match the workers scope and triggers an `oncontrollerchange` event for
-//     // the clients.
-//     return self.clients.claim();
-// });
+self.addEventListener('activate', function (event) {
+    // `claim()` sets this worker as the active worker for all clients that
+    // match the workers scope and triggers an `oncontrollerchange` event for
+    // the clients.
+    return self.clients.claim();
+});
 
 
 // Cache first then Network 
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     // Try the cache
-//     caches.match(event.request).then(function(response) {
-//       // Fall back to network
-//       return response || fetch(event.request);
-//     }).catch(function() {
-//       // If both fail, show a generic fallback:
-//       return caches.match('/Offline.html');
-//     })
-//   );
-// });
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    // Try the cache
+    caches.match(event.request).then(function(response) {
+      // Fall back to network
+      return response || fetch(event.request);
+    }).catch(function() {
+      // If both fail, show a generic fallback:
+      return caches.match('/Offline.html');
+    })
+  );
+});
 
 
 // Cache first then network and cache response
@@ -78,13 +79,13 @@ if ("serviceWorker" in navigator) {
 // });
 
 // Network first then cache
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request);
-    })
-  );
-});
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     fetch(event.request).catch(function() {
+//       return caches.match(event.request);
+//     })
+//   );
+// });
 
 
 
