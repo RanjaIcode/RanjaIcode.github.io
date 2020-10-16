cba2e42dafb106d4098b9ef311b3dc5df475a14e
RanjaIcode <ranjabosek@hotmail.com>
2020-10-16 20:49:07 +0200

    next

--- js/site.js
+++ js/site.js
@@ -1,7 +1,7 @@
 var dateien;
 $(function ()
 { 
-  $(".btnmode").css("color", "green");
+
 
   // Notifications
   Notification.requestPermission(function(status) {
@@ -56,7 +56,7 @@ if (typeof document.hidden !== "undefined") {
 function handleVisibilityChange() {
   var timeBadge = new Date().toTimeString().split(' ')[0];
   var newState = document.createElement('p');
-  newState.innerHTML = '' + timeBadge + ' Page visibility changed to ' + (document[hidden] ? 'hidden' : 'visible') + '.';
+  newState.innerHTML = '' + timeBadge + ' Sichtbarkeit änderte sich zu' + (document[hidden] ? 'hidden' : 'visible') + '.';
   target.appendChild(newState);
 }
 
@@ -68,54 +68,37 @@ if (hidden in document) {
 // LOCATION
 
 
-    getLocation();
-
-
+var x = document.getElementById("demo");
 
 function getLocation() {
-    var x = document.getElementById("demo");
-  
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition, showError);
-    } 
-    else { 
-      x.innerHTML = "Geolocation is not supported by this browser.";
-    }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
   }
-  
- 
+}
 
-  function showPosition(position) {
-    var x = document.getElementById("demo");
-  
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude;
-    var latlon = position.coords.latitude + "," + position.coords.longitude;
-  
-    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false&key=YOUR_KEY";
-  
-    $("#mapholder").innerHTML = "<img src='"+img_url+"'>";
-  }
-  
-  function showError(error) {
-    var x = document.getElementById("demo");
-  
-    switch(error.code) {
-      case error.PERMISSION_DENIED:
-        x.innerHTML = "User denied the request for Geolocation."
-        break;
-      case error.POSITION_UNAVAILABLE:
-        x.innerHTML = "Location information is unavailable."
-        break;
-      case error.TIMEOUT:
-        x.innerHTML = "The request to get user location timed out."
-        break;
-      case error.UNKNOWN_ERROR:
-        x.innerHTML = "An unknown error occurred."
-        break;
-    }
-  }
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
 
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      x.innerHTML = "User denied the request for Geolocation."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML = "Location information is unavailable."
+      break;
+    case error.TIMEOUT:
+      x.innerHTML = "The request to get user location timed out."
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML = "An unknown error occurred."
+      break;
+  }
+}
   
 //   File Upload
 	function dateiauswahl(evt) {
