d7cecbddff215d26a4b45881fb8e33006dd3d03f
RanjaIcode <ranjabosek@hotmail.com>
2020-10-16 21:44:24 +0200

    next

--- js/site.js
+++ js/site.js
@@ -4,33 +4,35 @@ $(function ()
 
 
   // Notifications
-  Notification.requestPermission(function(status) {
-    console.log('Notification permission status:', status);
-});
+//   Notification.requestPermission(function(status) {
+//     console.log('Notification permission status:', status);
+// });
 
-function displayNotification() {
-  if (Notification.permission == 'granted') {
-    navigator.serviceWorker.getRegistration().then(function(reg) {
-      var options = {
-        body: 'Here is a notification body!',
-        icon: 'img/logo_192.png',
-        vibrate: [100, 50, 100],
-        data: {
-          dateOfArrival: Date.now(),
-          primaryKey: 1
-        },
-        actions: [
-          {action: 'explore', title: 'Explore this new world',
-            icon: 'img/logo_192.png'},
-          {action: 'close', title: 'Close notification',
-            icon: 'img/logo_192.png'},
-        ]
-      };
-      reg.showNotification('Hello world!', options);
-    });
-  }
-}
-displayNotification();
+// function displayNotification() {
+//   if (Notification.permission == 'granted') {
+//     navigator.serviceWorker.getRegistration().then(function(reg) {
+//       var options = {
+//         body: 'Here is a notification body!',
+//         icon: 'img/logo_192.png',
+//         vibrate: [100, 50, 100],
+//         data: {
+//           dateOfArrival: Date.now(),
+//           primaryKey: 1
+//         },
+//         actions: [
+//           {action: 'explore', title: 'Explore this new world',
+//             icon: 'img/logo_192.png'},
+//           {action: 'close', title: 'Close notification',
+//             icon: 'img/logo_192.png'},
+//         ]
+//       };
+//       reg.showNotification('Hello world!', options);
+//     });
+//   }
+// }
+// displayNotification();
+
+
 
 
 
