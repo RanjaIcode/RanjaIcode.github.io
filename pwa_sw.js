cdfb284ba5bf1d9760e81463627a369d8e5541d7
RanjaIcode <ranjabosek@hotmail.com>
2020-10-17 14:48:33 +0200

    next

--- pwa_sw.js
+++ pwa_sw.js
@@ -91,3 +91,10 @@ self.addEventListener('fetch', function(event) {
 
   
 
+
+  this.scope.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    if (clients.openWindow && event.notification.data.url) {
+        event.waitUntil(clients.openWindow(event.notification.data.url));
+    }
+});
