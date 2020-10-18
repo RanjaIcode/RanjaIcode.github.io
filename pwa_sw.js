7e62777b6f3523d046740f6975fa9aa474f719c2
RanjaIcode <ranjabosek@hotmail.com>
2020-10-18 16:18:19 +0200

    next

--- pwa_sw.js
+++ pwa_sw.js
@@ -8,12 +8,39 @@ if ("serviceWorker" in navigator) {
       .register("pwa_sw.js")
       .then(function (reg) {
         console.log("Service worker has been registered for scope: " + reg.scope);
+          reg.pushManager.getSubscription().then(function(sub) {
+            if (sub === null) {
+              // Update UI to ask user to register for Push
+              console.log('Not subscribed to push service!');
+              subscribeUser();
+            } else {
+              // We have a subscription, update the database
+              console.log('Subscription object: ', sub);
+            }
+          });
       }).catch(err => {
         console.log("registration failed: ", err);
       });
   }
 }
- 
+function subscribeUser() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then(function(reg) {
+
+      reg.pushManager.subscribe({
+        userVisibleOnly: true
+      }).then(function(sub) {
+        console.log('Endpoint URL: ', sub.endpoint);
+      }).catch(function(e) {
+        if (Notification.permission === 'denied') {
+          console.warn('Permission for notifications was denied');
+        } else {
+          console.error('Unable to subscribe to push', e);
+        }
+      });
+    })
+  }
+}
 
                             /*  Offline Cache  */
   const CACHE = "v1";
